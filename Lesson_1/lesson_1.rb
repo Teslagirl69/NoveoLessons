7290889bdb7e5eb82b23f524bf3a39a7af5471ca
Teslagirl69 <teslagirl17@gmail.com>
2022-07-08 16:51:58 +0300

    remove "total"

--- Lesson_1/lesson_1.rb
+++ Lesson_1/lesson_1.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 def sum(arr)
-  total = 0
-
   if arr == []
     0
   else
-    total + arr.shift + sum(arr)
+  arr.shift + sum(arr)
   end
 end
-puts sum([1, 2, 3, 4])
+puts sum([1, 2, 3, 4, 5])
