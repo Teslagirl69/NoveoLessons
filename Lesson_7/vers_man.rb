66a1413f6cc246479aea4ab9ab2f743f4216e0ae
Teslagirl69 <teslagirl17@gmail.com>
2022-07-29 18:46:25 +0300

    add rollback in vers_man.rb

--- Lesson_7/vers_man.rb
+++ Lesson_7/vers_man.rb
@@ -18,43 +18,58 @@
 # vm.release # возвращает текущую версию в формате “{major}.{minor}.{patch}”
 require 'pry'
 class VersionManager
-  attr_accessor :version, :major, :minor, :patch
+  attr_accessor :version, :major, :minor, :patch, :actions
 
   def initialize(version = '0.0.1')
     @version = version
     raise 'Invalid version' if arr = version.split('.').any?(/\D/)
 
     @major, @minor, @patch = version.split('.')[0..2].map(&:to_i)
+    @actions = []
   end
 
   def major!
     @major += 1
     @minor = 0
     @patch = 0
+    actions << 'major'
   end
 
   def minor!
     @minor += 1
     @patch = 0
+    actions << 'minor'
   end
 
   def patch!
     @patch += 1
+    actions << 'patch'
   end
 
-  # def rollback!
-  #   raise 'Can not rollback to previous version' # if ...
-  # end
+   def rollback!
+     if actions.last == 'patch'
+      @patch -= 1
+    elsif actions.last == 'minor'
+      @patch = 0
+      @minor -= 1
+    elsif actions.last == 'major'
+      @patch = 0
+      @minor = 0
+      @major -= 1
+    else
+
+      raise 'Can not rollback to previous version'
+    end
+   end
 
   def release
-    "#{major}.#{minor}.#{patch}"
+    puts "#{major}.#{minor}.#{patch}"
   end
 end
 
 vm = VersionManager.new
-puts vm.major! # увеличивает мажорную версию, устанавливая минорную и патч в 0 (напр. “2.0.0”)
-puts vm.minor! # увеличивает минорную версию, устанавливая патч в 0 (напр. “2.1.0”)
-puts vm.patch! # увеличивает патч версию (напр. “2.1.1”)
-
-# puts vm.rollback! # возвращается к прошлой версии, если это возможно, иначе выдать ошибку
-puts vm.release # возвращает текущую версию в формате “{major}.{minor}.{patch}”
+ vm.major! # увеличивает мажорную версию, устанавливая минорную и патч в 0 (напр. “2.0.0”)
+ vm.minor! # увеличивает минорную версию, устанавливая патч в 0 (напр. “2.1.0”)
+ vm.patch! # увеличивает патч версию (напр. “2.1.1”)
+ vm.rollback! # возвращается к прошлой версии, если это возможно, иначе выдать ошибку
+ vm.release # возвращает текущую версию в формате “{major}.{minor}.{patch}”
