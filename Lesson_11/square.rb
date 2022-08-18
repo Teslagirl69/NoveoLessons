3d16132e681dab3e92b0ad9285e06dfedb66ab16
Teslagirl69 <teslagirl17@gmail.com>
2022-08-18 23:18:22 +0300

    fix style, attr, MATH

--- Lesson_11/square.rb
+++ Lesson_11/square.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 # Требуется сортировать фигуры по их площади.
 # Из фигур есть: Square (площадь - “квадрат” сторон),
@@ -5,78 +6,58 @@
 # Triangle (основание * высоту / 2),
 # Круг (pi * (R^2)), и произвольная
 # CustomShape (площадь просто задается).
-
 # Пример того, как должна выполняться сортировка элементов массива, если все элементы в этом массиве - фигуры
+
 class Square
-  attr_reader :side
   def initialize(side)
     @side = side
   end
+
   def square
-    @sq = side**2
-  end
-  def to_s
-   "Square with area:      #{@sq.ceil(2)}"
+    @sq = @side**2
   end
 end
 
 class Rectangle
- attr_reader :width, :length
   def initialize(width, length)
     @width = width
     @length = length
   end
+
   def square
-    @sq = width * length
+    @sq = @width * @length
   end
-  def to_s
-   "Rectangle with area:   #{@sq.ceil(2)}"
-  end
-
 end
-class Triangle
 
-attr_reader :base, :height
+class Triangle
   def initialize(base, height)
     @base = base
     @height = height
   end
+
   def square
-    @sq = base * height / 2
-  end
-def to_s
-   "Triangle with area:    #{@sq.ceil(2)}"
+    @sq = @base * @height / 2
   end
 end
 
-
 class Circle
-  include Math
-attr_reader :radius
   def initialize(radius)
     @radius = radius
   end
-  def square
-    @sq = PI * (radius**2)
-  end
-  def to_s
-   "Circle with area:      #{@sq.ceil(2)}"
-  end
 
+  def square
+    @sq = Math::PI * (@radius**2)
+  end
 end
 
-
 class CustomShape
-attr_reader :square
+  attr_reader :square
+
   def initialize(square)
     @square = square
   end
-  def to_s
-   "CustomShape with area: #{@square.ceil(2)}"
-  end
-
 end
 
-shapes = [Square.new(4), Rectangle.new(1,4), Triangle.new(2,6), Circle.new(3), CustomShape.new(11)]
+shapes = [Square.new(4), Rectangle.new(1, 4), Triangle.new(2, 6), Circle.new(3), CustomShape.new(11)]
 
-puts shapes.sort {|a, b| b.square <=> a.square  }
+puts shapes.sort { |a, b| b.square <=> a.square }
